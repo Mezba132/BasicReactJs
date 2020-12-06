8bb946a4047e72a32c0e227fd0cd64b747975aee
LeoMezba <csmezba@gmail.com>
2020-12-06 22:09:22 +0600

    Add Handling Event & Manipulate the state

--- src/App.js
+++ src/App.js
@@ -9,10 +9,21 @@ class App extends Component {
       {name:"Razib",profession:"Businessman"}
     ]
   }
+  switchPersonHandler = () => {
+    this.setState ({
+      person : [
+        {name:"Razib",profession:"developer"},
+        {name:"Leo",profession:"Businessman"}
+      ]
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
         <h1>Hello Guys</h1>
+        <button onClick={this.switchPersonHandler}>Switch Button</button>
         <Person name={this.state.person[0].name} profession={this.state.person[0].profession}>My Hobbies : Traveling</Person>
         <Person name={this.state.person[1].name} profession={this.state.person[1].profession}/>
       </div>
