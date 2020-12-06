6fdc17cb5fe9da89b5240f4e9e4a39720f53fb11
LeoMezba <csmezba@gmail.com>
2020-12-06 23:34:54 +0600

    Hook - useState()

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/person';
 
-class App extends Component {
-  state = {
+const app = props => {
+
+  const [personState, setPersonState] = useState({
     person : [
       {name:"Leo",profession:"developer"},
       {name:"Razib",profession:"Businessman"}
     ]
-  }
-  switchPersonHandler = () => {
-    this.setState ({
+  })
+
+  const [otherState, setOtherState] = useState("if you don't know,Now you know")
+
+  const switchPersonHandler = () => {
+    setPersonState ({
       person : [
         {name:"Razib",profession:"developer"},
         {name:"Leo",profession:"Businessman"}
       ]
     })
-  }
+  };
 
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello Guys</h1>
-        <button onClick={this.switchPersonHandler}>Switch Button</button>
-        <Person name={this.state.person[0].name} profession={this.state.person[0].profession}>My Hobbies : Traveling</Person>
-        <Person name={this.state.person[1].name} profession={this.state.person[1].profession}/>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hello Guys</h1>
+      <button onClick={switchPersonHandler}>Switch Button</button>
+      <Person name={personState.person[0].name} profession={personState.person[0].profession}>My Hobbies : Traveling</Person>
+      <Person name={personState.person[1].name} profession={personState.person[1].profession}/>
+    </div>
+  );
   
 }
 
-export default App;
+export default app;
+
+
