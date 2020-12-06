b8595cb37e562f0b304e4c57bc2c12f85556697b
LeoMezba <csmezba@gmail.com>
2020-12-06 21:56:11 +0600

    define state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import Person from './Person/person';
 
 class App extends Component {
+  state = {
+    person : [
+      {name:"Leo",profession:"developer"},
+      {name:"Razib",profession:"Businessman"}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hello Guys</h1>
-        <Person name="mezba" profession="developer">My Hobbies : Traveling</Person>
+        <Person name={this.state.person[0].name} profession={this.state.person[0].profession}>My Hobbies : Traveling</Person>
+        <Person name={this.state.person[1].name} profession={this.state.person[1].profession}/>
       </div>
     );
   }
--- src/Person/person.js
+++ src/Person/person.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const person = (props) => {
     return <div>
-        <h3>I am {props.name} and a professional full-stack {props.profession}</h3>
+        <h3>I am {props.name} and a professional {props.profession}</h3>
         <p>{props.children}</p>
     </div>
 }
