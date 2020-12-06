cc4eb1a333619020ec64eeab9f950a38e1c5955f
LeoMezba <csmezba@gmail.com>
2020-12-06 20:38:24 +0600

    creating a functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello Guys</h1>
+        <Person />
       </div>
     );
   }
---
+++ src/Person/person.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const person = () => {
+    return <div>
+        <h3>I am mezba and a professional full-stack developer</h3>
+    </div>
+}
+
+export default person;
